56256952300252714c630c90d2b44a17bc78ece0
Md Mansoor Kooripparambil <mdmansoor.k@maybank.com>
2020-03-16 11:54:10 +0800

    added generate link feature

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
-var _0x77c6=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x70\x69\x2E\x77\x68\x61\x74\x73\x61\x70\x70\x2E\x63\x6F\x6D\x2F\x73\x65\x6E\x64\x3F\x70\x68\x6F\x6E\x65\x3D","\x76\x61\x6C","\x69\x6E\x70\x75\x74\x23\x6E\x75\x6D\x62\x65\x72","\x26\x74\x65\x78\x74\x3D","\x74\x65\x78\x74\x61\x72\x65\x61\x23\x6D\x65\x73\x73\x61\x67\x65","\x63\x68\x61\x6E\x67\x65\x20\x70\x61\x73\x74\x65\x20\x6B\x65\x79\x75\x70","\x6F\x6E","\x23\x6E\x75\x6D\x62\x65\x72\x2C\x20\x23\x6D\x65\x73\x73\x61\x67\x65","\x23\x6E\x75\x6D\x62\x65\x72","\x74\x65\x73\x74","\x5F\x62\x6C\x61\x6E\x6B","\x6F\x70\x65\x6E","\x73\x68\x6F\x77","\x23\x68\x69\x6E\x74","\x68\x69\x64\x65","\x69\x6E\x70\x75\x74","\x72\x65\x64\x5F\x74\x65\x78\x74","\x72\x65\x6D\x6F\x76\x65\x43\x6C\x61\x73\x73","\x67\x72\x65\x65\x6E\x5F\x74\x65\x78\x74","\x61\x64\x64\x43\x6C\x61\x73\x73","\x64\x69\x73\x61\x62\x6C\x65\x5F\x63\x6C\x69\x63\x6B","\x23\x73\x65\x6E\x64","\x64\x69\x73\x61\x62\x6C\x65\x64","\x61\x74\x74\x72"];var link;function updateLink(_0xb0e6x3,_0xb0e6x4){link= _0x77c6[0]+ $(_0x77c6[2])[_0x77c6[1]]()+ _0x77c6[3]+ $(_0x77c6[4])[_0x77c6[1]]()}$(_0x77c6[7])[_0x77c6[6]](_0x77c6[5],function(){updateLink()});function go(){if(/^([+])?\d{11,13}$/[_0x77c6[9]]($(_0x77c6[8])[_0x77c6[1]]())){window[_0x77c6[11]](link,_0x77c6[10])}else {$(_0x77c6[13])[_0x77c6[12]]()}}$(_0x77c6[13])[_0x77c6[14]]();$(_0x77c6[8])[_0x77c6[6]](_0x77c6[15],function(){if(/^([+])?\d{11,13}$/[_0x77c6[9]]($(_0x77c6[8])[_0x77c6[1]]())){$(_0x77c6[8])[_0x77c6[17]](_0x77c6[16]);$(_0x77c6[8])[_0x77c6[19]](_0x77c6[18]);$(_0x77c6[21])[_0x77c6[17]](_0x77c6[20]);$(_0x77c6[21])[_0x77c6[23]](_0x77c6[22],false);$(_0x77c6[13])[_0x77c6[14]]()}else {$(_0x77c6[8])[_0x77c6[17]](_0x77c6[18]);$(_0x77c6[8])[_0x77c6[19]](_0x77c6[16]);$(_0x77c6[21])[_0x77c6[19]](_0x77c6[20]);$(_0x77c6[21])[_0x77c6[23]](_0x77c6[22],true);$(_0x77c6[13])[_0x77c6[12]]()}})
+var link;
+ function updateLink(x, y) {
+     link = "https://api.whatsapp.com/send?phone=" + $('input#number').val() + "&text=" + $('textarea#message').val();
+     //  var a = document.getElementById('send'); //or grab it by tagname etc
+     //  a.href = link;         
+ }
+ $("#number, #message").on("change paste keyup", function () {
+     updateLink();
+ });
+ 
+ function go() {
+     // window.location = link;
+     if (/^([+])?\d{11,13}$/.test($("#number").val())) {
+         window.open(link, '_blank');
+     }else
+     $('#hint').show(); 
+ }
+ function generate() {
+    // window.location = link;
+    if (/^([+])?\d{11,13}$/.test($("#number").val())) {
+        $('#link_details').show(); 
+        $("#link").html(link);
+    }else
+    $('#hint').show(); 
+}
+function copy() {
+    var copyText = document.getElementById("link");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Copied link: " + copyText.value);
+  }
+ $('#hint').hide();
+ $('#link_details').hide();
+ $('#number').on('input', function () {
+     if (/^([+])?\d{11,13}$/.test($("#number").val())) {
+
+         $('#number').removeClass('red_text');
+         $('#number').addClass('green_text');
+
+         $('#send').removeClass('disable_click');
+         $('#send').attr('disabled', false);
+         $('#hint').hide();
+     } else {
+
+         $('#number').removeClass('green_text');
+         $('#number').addClass('red_text');
+
+         $('#send').addClass('disable_click');
+         $('#send').attr('disabled', true);
+
+         $('#hint').show();
+     }
+
+ });
+ 
