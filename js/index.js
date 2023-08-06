8021b27ac70cd6e69741b7d139a8fb08fa33447a
Mohammed <pingtomansoor@gmail.com>
2023-08-06 23:46:59 +0800

    redirect to wsapme site

--- js/index.js
+++ js/index.js
@@ -18,8 +18,10 @@ var link;
  function generate() {
     // window.location = link;
     if (/^([+])?\d{11,13}$/.test($("#number").val())) {
-        $('#link_details').show(); 
-        $("#link").html(link);
+        // $('#link_details').show(); 
+        // $("#link").html(link);
+        wsapme_page_link = "https://wsap.me/_pages/home.html?mobile=" + $('input#number').val() + "&text=" + $('textarea#message').val();
+        window.open(wsapme_page_link, '_blank');
     }else
     $('#hint').show(); 
 }
